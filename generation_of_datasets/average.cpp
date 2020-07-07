3f418ab4a22c6763f276b7935b186fbffe7bed35
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2020-07-07 08:53:36 +0200

    Updated license header

--- generation_of_datasets/average.cpp
+++ generation_of_datasets/average.cpp
@@ -1,3 +1,32 @@
+/*********************************************************************
+ *
+ * Optimality Syntactic Dependency Distances
+ * 
+ * Copyright (C) 2020 Juan Luis Esteban
+ *
+ * This file is part of Optimality Syntactic Dependency Distances.
+ *
+ * Optimality Syntactic Dependency Distances is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Optimality Syntactic Dependency Distances is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Linear Arrangement Library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact:
+ * 
+ *     Juan Luis Esteban (esteban@cs.upc.edu)
+ *         Office 110, Omega building     Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.    CATALONIA, SPAIN
+ *         Webpage: https://www.cs.upc.edu/~esteban/
+ *
+ ********************************************************************/
+
 #include <cmath>
 #include <cmath>
 #include <iostream> // #include <fstream>
--- generation_of_datasets/fast_prediction_of_crossings_at_constant_sum_of_lengths.hpp
+++ generation_of_datasets/fast_prediction_of_crossings_at_constant_sum_of_lengths.hpp
@@ -1,4 +1,34 @@
-#ifndef _FAST_PREDICTIONS_OF_CROSSINGS_AT_CONSTANT_SUM_OF_LENGTHS_
+/*********************************************************************
+ *
+ * Optimality Syntactic Dependency Distances
+ * 
+ * Copyright (C) 2020 Juan Luis Esteban
+ *
+ * This file is part of Optimality Syntactic Dependency Distances.
+ *
+ * Optimality Syntactic Dependency Distances is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Optimality Syntactic Dependency Distances is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Linear Arrangement Library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact:
+ * 
+ *     Juan Luis Esteban (esteban@cs.upc.edu)
+ *         Office 110, Omega building     Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.    CATALONIA, SPAIN
+ *         Webpage: https://www.cs.upc.edu/~esteban/
+ *
+ ********************************************************************/
+
+#ifndef _FAST_PREDICTIONS_OF_CROSSINGS_AT_CONSTANT_SUM_OF_LENGTHS
+#define _FAST_PREDICTIONS_OF_CROSSINGS_AT_CONSTANT_SUM_OF_LENGTHS
 
 #include "undirected_graph.hpp"
 
